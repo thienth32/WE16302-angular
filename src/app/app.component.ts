d6462db7c05fefd35ecfdbe22d005d9db2e4adab
thienth32 <64114903+thienth32@users.noreply.github.com>
2022-03-14 10:52:36 +0700

    sử dụng httpclient để gửi request lấy dữ liệu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  // tạo 1 danh sách emenies gồm các object 
-  // có thuộc tính: code, name, avatar, heathling (số)
-  // amor (giáp)
-  // hiển thị danh sách enemies ở phía dưới của danh sách heroes
-  // thực hiện chức năng xóa enemies
-
-  heroes: Array<any> = [
-    {
-      code: "sgk",
-      name: "Songoku",
-      avatar: "https://i.pinimg.com/736x/d7/bd/23/d7bd2389fd03889edc309504c3377409.jpg",
-      gender: "Nam"
-    },
-    {
-      code: "mab",
-      name: "Ma bư",
-      avatar: "https://snkrvn.com/wp-content/uploads/2018/01/maxresdefault-960x640.jpg",
-      gender: "Nam"
-    }
-  ];
+export class AppComponent implements OnInit{
+  heroes: Array<any> = [];
   formHero: any = {
     code: "",
     name: "",
     avatar: "",
     gender: "Nữ"
   };
+  constructor(private http: HttpClient){}
+  ngOnInit(): void {
+    this.http.get<any>("http://localhost:3000/heroes")
+        .subscribe(data => {
+          this.heroes = data;
+        })
+  }
+
+  
 
   remove(hero: any){
     this.heroes = this.heroes.filter(item => item.code != hero.code);
